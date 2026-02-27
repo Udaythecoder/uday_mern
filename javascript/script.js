446d1f198ba4534745a2b14718778be9388b27bc
uday <malayudaykiran@gmail.com>
2026-02-27 16:06:44 +0530

    completed javascript

---
+++ javascript/script.js
@@ -0,0 +1,55 @@
+/*console.log("Hello World!!!!")
+var a;//globel scope
+a=9;
+var a;
+a=1;
+let b=8;//block scope
+ b=9;
+ const c=3.14;//block
+console.log(a);
+console.log(b);
+console.log(a+b);
+var str="santhosh";//``,'',""
+console.log(str);
+console.log(typeof str);
+var bool=true;
+var un;//undefined
+var nu=null;
+var big=123455667n;
+var symbol=Symbol('li');
+console.log(typeof big)
+//arr
+var arr=[1,2,3];
+console.log(arr);
+//dict--object
+var obj={name:"uday",roll:"G8"};
+console.log(obj);
+//arithematic operators
+var a=10;
+var b="5";
+console.log(a+b);
+console.log(a-b);
+console.log(a*b);
+console.log(a/b);
+console.log(a%b);
+console.log(a**b);
+//relational operators
+var a=10;
+var b=20;
+console.log(a==b);
+console.log(a===b);
+console.log(a!=b);
+console.log(a!==b);
+console.log(a>=b);
+console.log(a<=b);
+console.log(a>b);
+console.log(a<b);
+//logical operators
+var a=true;
+var b=false;
+console.log(a&&b);
+console.log(a||b);
+console.log(!a);*/
+
+
+
